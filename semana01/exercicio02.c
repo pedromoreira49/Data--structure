6745617d8c1be987094256350b84372d02dc4e1e
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-10-01 16:43:57 -0300

    Iniciado o exercicio 2

---
+++ semana01/exercicio02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Exercicio: 1.2
+Escreva uma função chamada exibeDivisores que exiba na tela os divisores do inteiro passado com argumento.
+Escreva um programa em C para imprimir os números de 1 a 20 com seus respectivos divisores.
+Cada conjunto de divisores deve ser impresso com a função exibeDivisores.
+*/
+
+int main(){
+	return 0;
+}
