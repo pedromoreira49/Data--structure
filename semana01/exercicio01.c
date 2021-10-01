5aa0f340bbb7eb5794f60c141c86bc9b90924f4f
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-10-01 11:20:08 -0300

    Informando o que o exercicio01 deverá realizar

--- semana01/exercicio01.c
+++ semana01/exercicio01.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+/*Exercicio: 1.1
+Escreva um programa em C para ler um valor Q que representa a quantidade de alunos de uma turma.
+A seguir ler Q pares de dados contendo, cada um, a altura e um código para masculino (1) e feminino (2) de cada aluno.
+Calcular e escrever:
+-A altura do "ALUNO" mais alto da turma.
+-A média de altura das "ALUNAS".
+*/
+
 int main(){
 	int q;
 	printf("Informe a quantidade de alunos:\n");
 	scanf("%d", &q);
 
-	
+
 	return 0;
 }
