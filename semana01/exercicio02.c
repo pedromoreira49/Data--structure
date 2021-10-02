060b2dc5ca9324526717e9bbd5e731ecb6947465
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-10-02 17:14:47 -0300

    Finalizado exercicio 2.

--- semana01/exercicio02.c
+++ semana01/exercicio02.c
@@ -7,6 +7,24 @@ Escreva um programa em C para imprimir os números de 1 a 20 com seus respectivo
 Cada conjunto de divisores deve ser impresso com a função exibeDivisores.
 */
 
+void exibeDivisores(int num);
+
 int main(){
+	int num;
+	printf("Informe um numero:\n");
+	scanf("%d", &num);
+	for(int i = 1; i <= num; i++){
+		printf("%d: ", i);
+		exibeDivisores(i);
+		printf("\n");
+	}
 	return 0;
+}
+
+void exibeDivisores(int num){
+	for(int i = 1; i <= num; i++){
+		if((num % i) == 0){
+			printf("%d ", i);
+		}
+	}
 }
