d7b7d5df2b6f47e86564f9a236c98b010567af19
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-10-02 19:57:24 -0300

    Iniciado exercicio 05

---
+++ semana01/exercicio05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Exercicio: 1.5
+Escreva um função em C chamada duplaCrescente que classifique 2 valores inteiros passados como argumento em ordem crescente.
+Entrada: Dois números inteiros
+Saída: Os dois inteiros em ordem crescente.
+Retorno: Nenhum
+Escreva um programa em C para ler uma quantidade indeterminada de duplas de inteiros.
+Escrever cada dupla em ordem crescente. Os valores em ordem crescente devem ser obtidos através da chamada à função duplaCrescente.
+O programa termina quando os dois valores informados forem iguais (nesta situação os valores não devem ser impressos).
+*/
+
+
+
+int main(){
+	return 0;
+}
