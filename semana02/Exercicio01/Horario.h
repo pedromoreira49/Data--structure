96c38aef93b8da9637f282b4369e0725de705926
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-10-07 22:03:40 -0300

    Iniciando lista de exercicios da semana 2

---
+++ semana02/Exercicio01/Horario.h
@@ -0,0 +1,9 @@
+#ifndef Horario_H
+#define Horario_H
+
+typedef struct {
+	int hh, mm, ss;
+
+} Horario;
+
+#endif
---
+++ semana02/Exercicio01/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "Horario.h"
+
+/*Exercicio: 2.1
+Definir um tipo chamado Horario para armazenar um horário composto de: hora, minuto, segundo.
+Escreva um programa em C para ler dois horários. Escreva o menor horário no formato HH:MM:SS. No caso de igualdade escrever a mensagem "Horários iguais".
+*/
+
+int main(void) {
+
+	return 0;
+}
+
