4cf5b4959a9bf453a3268766ad0fad5d86f56db6
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-10-11 18:31:58 -0300

    Informando o que o exercicio02 solicita.

--- semana02/Exercicio02/main.c
+++ semana02/Exercicio02/main.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Exercicio: 2.2
+Definir um tipo chamado Aluno para armazenar os seguintes dados de um aluno: número de matrı́cula e data de nascimento (tipo Data). Escrever um programa em C para ler a data de hoje armazenando-a em uma variável do tipo Data. A seguir ler uma quantidade indeterminada de dados de alunos (Aluno). Para cada aluno lido escrever se ele já completou 18 anos até a data informada. O programa termina ao ser informado um valor negativo para a matrı́cula. Nesta situação a data de nascimento não deve ser lida.
+*/
+
+
 int main(){
 	Date hoje, *p, *q;
 	Aluno aluno;
