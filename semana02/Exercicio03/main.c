b313370d855fd751627eb0085a4c9baa66508378
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-10-12 17:45:16 -0300

    Finalizado exercicio03

--- semana02/Exercicio03/main.c
+++ semana02/Exercicio03/main.c
@@ -1,27 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "conta.c"
+
+int printMenu(int op);
 
 int main(){
 
-	Conta cc;
+	Conta cc, cp;
 	float value;
-	int num;
+	int num, nump, op;
 
-	printf("Informe o numero da conta:\n");
+	printf("Informe o numero da conta corrente:\n");
 	scanf("%d", &num);
 	criarConta(&cc, num);
+	printf("Informe o numero da conta poupanca:\n");
+	scanf("%d", &nump);
+	criarConta(&cp, nump);
 
-	printf("Faca o primeiro deposito\n");
-	scanf("%f", &value);
-	depositaConta(&cc, value);
-
-	printf("Faca uma retirada\n");
-	scanf("%f", &value);
-	retiraConta(&cc, value);
-
-	printf("Veja seu saldo\n");
-	obtemSaldo(&cc);
-
+	while(op != 5){
+		printf("O que deseja fazer:\n1-Deposito conta corrente\n2-Deposito conta poupanca\n3-Retirada conta corrente\n4-Retirada conta poupanca\n5-Fim\n");
+		scanf("%d", &op);
+		switch(op){
+			case 1:
+				printf("Escolhido: Deposito conta corrente\n");
+				printf("Insira o valor:\n");
+				scanf("%f", &value);
+				depositaConta(&cc, value);
+				printf("Saldo atual: R$ %.2f\n", obtemSaldo(&cc));
+				break;
+			case 2:
+				printf("Escolhido: Deposito conta poupanca\n");
+				printf("Insira o valor:\n");
+				scanf("%f", &value);
+				depositaConta(&cp, value);
+				printf("Saldo atual: R$ %.2f\n", obtemSaldo(&cp));
+				break;
+			case 3:
+				printf("Escolhido: Retirada conta corrente\n");
+				printf("Insira o valor:\n");
+				scanf("%f", &value);
+				if(obtemSaldo(&cc) >= value){
+					retiraConta(&cc, value);
+				}else if(obtemSaldo(&cp) >= value){
+					retiraConta(&cp, value);
+				}else{
+					printf("Saldo insuficiente");
+				}
+				break;
+			case 4:
+				printf("Escolhido: Retirada conta poupanca\n");
+				printf("Insira o valor:\n");
+				scanf("%f", &value);
+				if(obtemSaldo(&cp) >= value){
+					retiraConta(&cp, value);
+				}else{
+					printf("Saldo insuficiente");
+				}
+				break;
+			case 5:
+				printf("Escolhido: Sair.\n");
+				exit(0);
+				break;
+		}
+	}
 	return 0;
 
 }
