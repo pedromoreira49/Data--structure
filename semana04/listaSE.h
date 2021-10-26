dfe5abaaf0d00b6d65b38b019b7037f4ba446855
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-10-26 16:37:38 -0300

    Dando inicio aos exercicios da semana04 com Alocação Dinâmina

---
+++ semana04/listaSE.h
@@ -0,0 +1,35 @@
+#ifndef listaSE_H
+#define listaSE_H
+
+#define SUCESSO 0
+#define LISTA_VAZIA 1
+#define FALTOU_MEMORIA 2
+#define CODIGO_INEXISTENTE 3
+
+typedef struct{
+	int cod;
+	float peso;
+} Dado;
+
+typedef struct nodo Nodo;
+
+struct nodo{
+	Dado info;
+	Nodo *prox;
+};
+
+typedef struct{
+	Nodo *inicio;
+} ListaSE;
+
+void criaLista(ListaSE *lt);
+void exibe(ListaSE lt);
+int estaVazia(ListaSE lt);
+int incluiNoFim(ListaSE *lt, Dado *d);
+int incluiDepois(ListaSE *lt, Dado *d, int cod);
+int consultaPorCodigo(ListaSE lt, Dado *d, int cod);
+int incluiNoInicio(ListaSE *lt, Dado *d);
+int quantidadeDeNodos(ListaSE lt);
+int excluiDoInicio(ListaSE *lt, Dado d);
+int excluiNodo(ListaSE *lt, Dado *d, int cod);
+
