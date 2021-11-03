1706fc84078b5af80a44a0d39f8b710f823d80b8
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-11-02 23:02:26 -0300

    implementado função excluiNodo

--- semana04/listase.c
+++ semana04/listase.c
@@ -107,20 +107,28 @@ int excluiDoFim(ListaSE *lt, Dado *d){
 		return SUCESSO;
 	}
 }
-/*
-int excluiNodo(ListaSE *lt, Dado d){
+
+int excluiNodo(ListaSE *lt, Dado *d, int cod){
 	Nodo *pNodo;
+	Nodo *pAux;
 	if(lt->inicio == NULL){
 		return LISTA_VAZIA;
 	}else{
 		pNodo = lt->inicio;
-		while(d != pNodo->info->cod){
-			pNodo = pNodo->info->cod;
+		while(pNodo->info.cod != cod){
+			pNodo = pNodo->prox;
 		}
-		
+		pAux = lt->inicio;
+		while(pAux->prox != pNodo){
+			pAux = pAux->prox;
+		}
+		*d = pNodo->info;
+		pAux->prox = pNodo->prox;
+		free(pNodo);
+		return SUCESSO;
 	}
 }
-*/
+
 int consultaPorCodigo(ListaSE lt, int cod){
 	Nodo *pNodo;
 	if(lt.inicio == NULL){
