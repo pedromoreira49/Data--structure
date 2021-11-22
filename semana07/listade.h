a1d2bbdb4d763a901c7f7d2a1413bfec59deaa9d
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-11-22 19:47:01 -0300

    Iniciado lista da semana07 sobre listas duplamente encadeadas

---
+++ semana07/listade.h
@@ -0,0 +1,37 @@
+#ifndef listade_h
+#define listade_h
+
+#define SUCESSO 0
+#define LISTA_VAZIA 1
+#define FALTOU_MEMORIA 2
+#define CODIGO_INEXISTENTE 3
+
+typedef struct {
+	int cod;
+	float peso;
+} Dado;
+
+typedef struct nodo Nodo;
+
+struct nodo {
+	Dado info;
+	Nodo *prox;
+};
+
+typedef struct {
+	Nodo *inicio;
+	Nodo *fim;
+	Nodo n;
+} Listade;
+
+void criaLista(Listade lt);
+void exibe(Listade lt);
+int estaVazia(Listade lt);
+int quantidadeDeNodos(Listade lt);
+int incluiNoInicio(Listade *lt, Dado d);
+int excluiDoInicio(Listade *lt, Dado *d);
+int incluiNoFim(Listade *lt, Dado d);
+int excluiDoFim(Listade *lt, Dado *d);
+
+
+#endif
