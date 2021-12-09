7f09788a1b347ac2e1a214474ed4384f36188229
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-12-09 14:53:43 -0300

    Iniciado trabalho final da disciplina

---
+++ trabalho-final/voo.h
@@ -0,0 +1,26 @@
+#ifndef voo_h
+#define voo_h
+
+#define SUCESSO 0
+#define FILA_VAZIA 1
+#define FALTOU_MEMORIA 2
+#define NUMERO_INEXISTENTE 3
+
+typedef struct {
+	int num;
+	char cia[10];
+} Voo;
+
+typedef struct nodo Nodo;
+
+struct nodo{
+	Voo v;
+	Nodo *prox;
+}
+
+typedef struct {
+	Nodo *frente;
+	Nodo *re;
+} Filase;
+
+#endif
