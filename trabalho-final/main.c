64fe81c0bc4bbd9d07c3923d948ad1355e0bb09c
pedromoreira49 <jpedrostonemoreira@gmail.com>
2021-12-13 16:11:47 -0300

    Arrumado erros de digitação no arquivo main

--- trabalho-final/main.c
+++ trabalho-final/main.c
@@ -12,13 +12,13 @@ int main(){
 		printf("Menu:\n");
 		printf("0. Fim             3. Consulta existência de voo\n");
 		printf("1. Insere um voo   4. Quantidade de avioes\n");
-		Printf("2. Retira um voo   5. Exibe todos os voos\n");
+		printf("2. Retira um voo   5. Exibe todos os voos\n");
 		scanf("%d", &op);
 		switch(op){
 			case 0:
 				exit(0);
 				break;
 		}
-	}
-
+	}while(op != 0);
+	return 0;
 }
